e30edac9759107ef1e08b8aba087757d87300cd3
TouyeeL <156361293+TouyeeL@users.noreply.github.com>
2025-02-27 01:16:07 +0000

    added more animations

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ const heart = gsap.timeline();
 
 // Animate the gradient stroke of the SVG
 gsap.to("#heart", {
-    duration: 5,
-    strokeDashoffset: 0,
+    duration: 0.5,
+    scale: 1.2,
+    transformOrigin: "center center",
     ease: "power1.inOut",
-    strokeDasharray: 1000,
     repeat: -1,
-    yoyo: true,
-    repeatDelay: 0.5,
+    yoyo: true
 });
 
 const star = gsap.timeline();
@@ -36,3 +35,21 @@ gsap.to("#star", {
 });
 
 
+const moon = gsap.timeline();
+
+gsap.to("#moon", {
+    
+    attr: {
+        stroke: "url(#gradient2)"
+    },
+    
+});
+
+const shine = gsap.timeline();
+shine.to("#shine", {
+    duration: 2,
+    scale: 1.5,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+});
